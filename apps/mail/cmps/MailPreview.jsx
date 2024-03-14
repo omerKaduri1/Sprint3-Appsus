5b32987f5acc6e5cb50a40816971113ee9b15d79
amitfrid7 <fridamit@gmail.com>
2024-03-14 20:45:39 +0200

    2100 Delivery

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -9,27 +9,34 @@ export function MailPreview({ mail, handleIsRead, onRemoveMail }) {
         mail.isRead = isRead
     }
 
-    return <li className={`flex space-between`}>
-        <div className="inputs flex">
-            <input type="checkbox" className="fa star" />
-            <input type="checkbox" />
-            <Link to={`/mails/${mail.id}`}>
-                <div className={`mail-from ${(isRead) ? '' : 'bold'}`}>
-                    {(mail.from) ? `${mail.from}` : 'Me'}
-                </div>
-            </Link>
-        </div>
-        <div className={`mail-subject flex space-between`}>
-            <div className={`${(isRead) ? '' : 'bold'}`}>
-                {mail.subject}
-            </div>
-            {mail.body.substring(0, 50)}
-        </div>
 
-        <div className="mail-actions">
-            <button className='fa envelope' onClick={handleIsRead}></button>
-            <button className='fa forward'></button>
-            <button className='fa trash' onClick={() => onRemoveMail(mail.id)}></button>
-        </div>
-    </li>
+    return (
+        <li className={`flex space-between pointer {${(isRead) ? 'read' : ''}`}>
+            <div className="inputs flex">
+                <input type="checkbox" className="fa star" />
+                <input type="checkbox" />
+                <div className={`mail-from ${(isRead) ? '' : 'bold'}`}>
+                    <Link to={`/mails/${mail.id}`}>
+                        {(mail.from) ? `${mail.from}` : 'Me'}
+                    </Link>
+                </div>
+            </div>
+            <div className={`mail-subject flex space-between`}>
+                <div className={`${(isRead) ? '' : 'bold'}`}>
+                    <Link to={`/mails/${mail.id}`}>
+                        {mail.subject}
+                    </Link>
+                </div>
+                <Link to={`/mails/${mail.id}`}>
+                    {mail.body.substring(0, 50)}
+                </Link>
+            </div>
+
+            <div className="mail-actions">
+                <button className='fa envelope' onClick={handleIsRead}></button>
+                <button className='fa forward'></button>
+                <button className='fa trash' onClick={() => onRemoveMail(mail.id)}></button>
+            </div>
+        </li >
+    )
 }

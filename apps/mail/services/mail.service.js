ee727fa2f6c422e135576a9e1a781e86ec3da38d
amitfrid7 <fridamit@gmail.com>
2024-03-16 15:43:32 +0200

    Fix MQ bugs

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -60,7 +60,6 @@ function send(mail) {
     return storageService.post(MAIL_KEY, mail)
 }
 
-
 function getEmptyMail() {
     const email = {
         id: utilService.makeId(),
@@ -97,23 +96,6 @@ function getFilterFromParams(searchParams = {}) {
     }
 }
 
-function _creatEmails() {
-    let mails = utilService.loadFromStorage(MAIL_KEY)
-    if (!mails || !mails.length) {
-        mails = []
-        mails.push(_creatEmail())
-        mails.push(_creatEmail())
-        mails.push(_creatEmail())
-        mails.push(_creatEmail())
-        mails.push(_creatEmail())
-        mails.push(_creatEmail())
-        mails.push(_creatEmail())
-        mails.push(_creatEmail())
-        utilService.saveToStorage(MAIL_KEY, mails)
-    }
-    return mails
-}
-
 function _creatEmail() {
     const mail = getEmptyMail()
     mail.subject = utilService.makeLorem(2)
@@ -131,4 +113,254 @@ function _setNextPrevmailId(mail) {
         mail.prevmailId = prevmail.id
         return mail
     })
+}
+
+function _creatEmails() {
+    let mails = utilService.loadFromStorage(MAIL_KEY)
+    if (!mails || !mails.length) {
+        mails = [
+            {
+                id: utilService.makeId(),
+                subject: 'omerKaduri1 invited you to omerKaduri1/Sprint3-Appsus',
+                body: utilService.makeLorem(100),
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'omerKaduri1',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: 'You\'re Invited. Apply today for $300 AWS credits',
+                body: utilService.makeLorem(123),
+                isRead: true,
+                sentAt: 0,
+                removedAt: null,
+                from: 'Amazon Web Services',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: 'Last week\'s updates in your shared folders',
+                body: utilService.makeLorem(94),
+                isRead: true,
+                sentAt: 0,
+                removedAt: null,
+                from: 'Dropbox',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: 'Welcome to TMDB!',
+                body: utilService.makeLorem(100),
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'The Movie Database (TMDB)',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: 'Code for signing in to Zoom',
+                body: utilService.makeLorem(100),
+                isRead: true,
+                sentAt: 0,
+                removedAt: null,
+                from: 'Zoom',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: 'Your verification link',
+                body: utilService.makeLorem(120),
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'Booking.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: 'Global Sale Extended ✨up to 40% OFF',
+                body: utilService.makeLorem(114),
+                isRead: true,
+                sentAt: 0,
+                removedAt: null,
+                from: 'Selina',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: 'הודעה מפורום הקורס "תורת המימון" בנושא "בהצלחה בבחינה "',
+                body: utilService.makeLorem(130),
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'The Open University',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+            {
+                id: utilService.makeId(),
+                subject: '',
+                body: '',
+                isRead: false,
+                sentAt: 0,
+                removedAt: null,
+                from: 'momo@momo.com',
+                to: 'user@appsus.com',
+                mailStatus: 'inbox'
+            },
+        ]
+
+
+
+
+
+
+
+
+
+
+
+
+        mails.push(_creatEmail())
+        mails.push(_creatEmail())
+        mails.push(_creatEmail())
+        mails.push(_creatEmail())
+        mails.push(_creatEmail())
+        mails.push(_creatEmail())
+        mails.push(_creatEmail())
+        mails.push(_creatEmail())
+        utilService.saveToStorage(MAIL_KEY, mails)
+    }
+    return mails
 }
